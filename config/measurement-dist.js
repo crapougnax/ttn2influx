0760d0728c87d7e989d3de1bf77be3da12ce40d7
Olivier Lépine <olivier@lepine.fr>
2019-03-14 21:29:30 +0100

    Add gitignore for config folder

--- config/measurement-dist.js
+++
@@ -1,8 +0,0 @@
-/**
- * Create key/value pairs here to match data key prefixes to measurements
- */
-export const measurements = {
-  measurement1: 'data_prefix_1',
-  measurement2: 'data_prefix_2',
-  measurement3: 'data_prefix_3',
-}
